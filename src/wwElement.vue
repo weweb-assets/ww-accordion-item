64608711445d61fc700c8c86f1a17a1c1e2c5dd6
Léo Coletta <mail.leo.coletta@gmail.com>
2024-11-12 21:07:16 +0100

    feat: Setup ww-accordion-item

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,16 +1,26 @@
 <template>
-    <div
-        :style="{
-            '--transition-duration': animationDurationValue + 'ms',
-            '--transition-easing': animationEasing,
-        }"
-        class="ww-accordion-root"
-        @keydown.esc="onEscapeKeyDown()"
-    ></div>
+    <div class="ww-accordion-item">
+        <wwElement
+            :uid="uid"
+            :content="content"
+            v-bind="content.triggerElement"
+            role="dialog"
+            class="ww-accordion-item-content"
+        />
+        <WwExpandTransition>
+            <div v-if="isExpanded" key="item-wrapper">
+                <wwElement v-bind="content.contentElement" role="dialog" class="ww-accordion-item-content" />
+            </div>
+        </WwExpandTransition>
+    </div>
 </template>
 
 <script>
+import { inject, provide, toRef, computed, watch } from 'vue';
+import WwExpandTransition from './wwExpandTransition.vue';
+
 export default {
+    components: { WwExpandTransition },
     props: {
         content: { type: Object, required: true },
         uid: { type: String, required: true },
@@ -18,9 +28,54 @@ export default {
         wwEditorState: { type: Object, required: true },
         /* wwEditor:end */
     },
-    emits: [],
-    setup() {},
+    emits: ['trigger-event'],
+    setup(props, { emit }) {
+        const { disabled } = toRef(() => props.content.disabled);
+
+        const { value } = inject('weweb-assets/ww-accordion-root');
+        const isExpanded = computed(() => value.value === props.content.value);
+
+        watch(isExpanded, val => {
+            emit('trigger-event', {
+                name: 'change',
+                event: {
+                    value: val,
+                },
+            });
+        });
+
+        function openAccordion() {
+            value.value = props.content.value;
+        }
+        function closeAccordion() {
+            value.value = null;
+        }
+        function toggleAccordion() {
+            const newValue = value.value === props.content.value ? null : props.content.value;
+            const { log } = console;
+            log('toggleAccordion', newValue, props.content.value);
+            value.value = newValue;
+        }
+
+        provide('weweb-assets/ww-accordion-item', {
+            openAccordion,
+            closeAccordion,
+            toggleAccordion,
+            disabled,
+            isExpanded,
+        });
+
+        return {
+            isExpanded,
+            parent,
+        };
+    },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.ww-accordion-item {
+    display: flex;
+    flex-direction: column;
+}
+</style>
